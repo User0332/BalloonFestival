0cfd3852344571a35a93c0d3775050d12cfe8d40
Amit Sail <amitsail2014@gmail.com>
2023-10-30 18:28:07 -0400

    changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 	const titleScreen = document.getElementById('titleScreen');
-    const cadetActionsSelect = document.getElementById('cadetActions');
-    const startButton = document.getElementById('startButton');
     const goBackButton = document.getElementById('goBackButton');
 
-	startButton.addEventListener('click', function () {
+	const cadetStartButton = document.getElementById('cadetStartButton');
+	const cadetRegistration = document.getElementById('cadetRegistration');
+	const cadetDataUpload = document.getElementById('cadetDataUpload');
+	const cadetDisplay = document.getElementById('cadetDisplay');
+
+	const seniorActions = document.getElementById('seniorActions');
+	const seniorRegistration = document.getElementById('seniorRegistration');
+	const seniorDataUpload = document.getElementById('seniorDataUpload');
+	const seniorDisplay = document.getElementById('seniorDisplay');
+
+	
+
+	cadetStartButton.addEventListener('click', function () {
         titleScreen.style.display = 'none';
         cadetActions.style.display = 'block';
+		seniorActions.style.display = 'block';
+
     });
 
 	goBackButton.addEventListener('click', function () {
