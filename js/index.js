92dd4a6a3295f17b3b5486a12716535b020b7bb8
Amit Sail <amitsail2014@gmail.com>
2023-10-30 17:32:00 -0400

    Fixed HTML, CSS, JS

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 	const titleScreen = document.getElementById('titleScreen');
-    const cadetActions = document.getElementById('cadetActions');
+    const cadetActionsSelect = document.getElementById('cadetActions');
     const startButton = document.getElementById('startButton');
     const goBackButton = document.getElementById('goBackButton');
-
 
 	startButton.addEventListener('click', function () {
         titleScreen.style.display = 'none';
         cadetActions.style.display = 'block';
     });
 
-    goBackButton.addEventListener('click', function () {
-        cadetActions.style.display = 'none';
+	goBackButton.addEventListener('click', function () {
         titleScreen.style.display = 'block';
+        cadetActions.style.display = 'none';
     });
+	
+
+	
