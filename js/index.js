34bb2a4e978f76243dbcba884144d4d0ae85d903
User0332 <carlzfurtado@gmail.com>
2023-10-30 20:43:33 -0400

    remove wrong js links, modularize js later

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
-	const mainPage = document.getElementById('mainPage');
-    const goBackButton = document.getElementById('goBackButton');
+const mainPage = document.getElementById('mainPage');
+const goBackButton = document.getElementById('goBackButton');
 
-	const cadetStartButton = document.getElementById('cadetStartButton');
-	const cadetRegistration = document.getElementById('cadetRegistration');
-	const cadetDataUpload = document.getElementById('cadetDataUpload');
-	const cadetDisplay = document.getElementById('cadetDisplay');
+const cadetStartButton = document.getElementById('cadetStartButton');
+const cadetRegistration = document.getElementById('cadetRegistration');
+const cadetDataUpload = document.getElementById('cadetDataUpload');
+const cadetDisplay = document.getElementById('cadetDisplay');
 
-	const seniorActions = document.getElementById('seniorActions');
-	const seniorRegistration = document.getElementById('seniorRegistration');
-	const seniorDataUpload = document.getElementById('seniorDataUpload');
-	const seniorDisplay = document.getElementById('seniorDisplay');
+const seniorActions = document.getElementById('seniorActions');
+const seniorRegistration = document.getElementById('seniorRegistration');
+const seniorDataUpload = document.getElementById('seniorDataUpload');
+const seniorDisplay = document.getElementById('seniorDisplay');
 
-	// cadetStartButton.addEventListener('click', function () {
-	// 	hideTitleScreen();
-    //     cadetActions.style.display = 'block';	
-	// });
+// cadetStartButton.addEventListener('click', function () {
+// 	hideTitleScreen();
+//     cadetActions.style.display = 'block';	
+// });
 
-	// goBackButton.addEventListener('click', function () {
-    //     titleScreen.style.display = 'block';
-    //     cadetActions.style.display = 'none';
-    // });
+// goBackButton.addEventListener('click', function () {
+//     titleScreen.style.display = 'block';
+//     cadetActions.style.display = 'none';
+// });
 
-	// function hideTitleScreen()
-	// {
-	// 	titleScreen.style.display = 'none';
-	// }
+// function hideTitleScreen()
+// {
+// 	titleScreen.style.display = 'none';
+// }
 
-	// function showTitleScreen()
-	// {
-	// 	titleScreen.style.display = 'block';
-	// }
-	
+// function showTitleScreen()
+// {
+// 	titleScreen.style.display = 'block';
+// }
 
-	
+
