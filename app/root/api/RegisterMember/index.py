7d02c9a06fc8aac787056154ec1f422fd735b72d
User0332 <carlzfurtado@gmail.com>
2024-01-14 19:40:01 -0500

    fix shift selection on registration

--- app/root/api/RegisterMember/index.py
+++ app/root/api/RegisterMember/index.py
@@ -84,21 +84,14 @@ class Volunteer:
 	def camping_saturday(self):
 		return self.camping[1]
 
-
 	def __str__(self) -> str:
-		return f"This is {'C/' if self.member_type == CADET_MEMBER else ''}{self.grade} {self.full_name} (Member #{self.CAPID}) from {self.squadron}. I am {self.age} years old and have been to the balloon festival {self.experience_level} time(s) before. I have committed to {len(self.blocks_committed)} block(s): {','.join(self.blocks_committed)}. I am a {'cadet' if self.member_type == CADET_MEMBER else 'senior member'}. My food preference is {FOOD_PREF_MAP[self.food_pref]}. My medical history is {len(self.med_hist)} characters long.{' I am ICUT certified.' if self.icut_cert else ''}{' I am a driver.' if self.driver else ''} I will be camping on {CAMPING_MAP[self.camping]}. My weighted experience is {self.weighted_exp}. I have completed {len(self.shifts_completed)} shift(s): {self.shifts_completed}."
+		return f"This is {'C/' if self.member_type == CADET_MEMBER else ''}{self.grade} {self.full_name} (Member #{self.CAPID}) from {self.squadron}. I am {self.age} years old and have been to the balloon festival {self.experience_level} time(s) before. I have committed to {len(self.blocks_committed)} block(s): {','.join(str(x) for x in self.blocks_committed)}. I am a {'cadet' if self.member_type == CADET_MEMBER else 'senior member'}. My food preference is {FOOD_PREF_MAP[self.food_pref]}. My medical history is {len(self.med_hist)} characters long.{' I am ICUT certified.' if self.icut_cert else ''}{' I am a driver.' if self.driver else ''} I will be camping on {CAMPING_MAP[self.camping]}. My weighted experience is {self.weighted_exp}. I have completed {len(self.shifts_completed)} shift(s): {','.join(str(x) for x in self.shifts_completed)}."
 
 
 def handler(app: webpy.App, *args):
 	from flask import request, redirect
 
-	print(request.form["blocks_committed"])
-
-	# blocks_committed = []
-
-	# for i in range(1, 17):
-	# 	if (json.loads(request.form[f"block{i}"])):
-	# 		blocks_committed.append(i)
+	blocks_committed = [int(block) for block in request.form.getlist("blocks_committed")]
 
 	volunteer = Volunteer(
 		int(request.form["CAPID"]),
@@ -111,9 +104,9 @@ def handler(app: webpy.App, *args):
 		blocks_committed,
 		int(request.form["food_pref"]),
 		request.form["med_hist"],
-		json.loads(request.form["icut_cert"]),
-		json.loads(request.form["driver"]),
-		(json.loads(request.form["camping_fri"]), json.loads(request.form["camping_sat"])),
+		bool(request.form.get("icut_cert")),
+		bool(request.form.get("driver")),
+		(bool(request.form.get("camping_fri")), bool(request.form.get("camping_sat"))),
 		int(request.form["experience_level"])
 	)
 
